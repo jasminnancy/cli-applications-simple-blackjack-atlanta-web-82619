39ef71278efd8f9dcc2ef52cc086eea738ce2238
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-20 18:10:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,9 +22,10 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(card_total)
+def initial_round
   deal_card
   deal_card
+  puts display_card_total
   return display_card_total
 end
 
