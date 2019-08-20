377642ba48807532a5deab0a26818ef4a4676180
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-20 18:49:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,12 +37,12 @@ def hit?(card_total)
     card_total += deal_card
   else
     invalid_command
-    prompt_user
   end
 end
 
 def invalid_command
     puts "Please enter a valid command"
+    prompt_user
 end
 
 #####################################################
